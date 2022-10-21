06918c522863d8d38d6c00d3a5c618015853b602
JulesHuisman <jules.huisman@quantile.nl>
2022-10-21 14:15:01 +0200

    Created dad joke script

---
+++ main.py
@@ -0,0 +1,13 @@
+from urllib.request import Request, urlopen
+
+def fetch_random_dad_joke() -> str:
+    req = Request(
+        url="https://icanhazdadjoke.com/",
+        headers={
+            "Accept": "text/plain",
+            "User-Agent": "Workshop"
+        }
+    )
+    return urlopen(req).read().decode("utf-8")
+
+print(fetch_random_dad_joke())
